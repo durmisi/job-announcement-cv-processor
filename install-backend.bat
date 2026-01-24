6c07e82bbdd92565f5e5ac734c22e8db6d6c1106
Admir Durmishi <durmisi@gmail.com>
2026-01-24 19:44:51 +0100

    fix: correct echo statement for backend installation confirmation

--- install-backend.bat
+++ install-backend.bat
@@ -3,4 +3,3 @@ cd /d %~dp0
 echo Installing/updating backend requirements...
 .venv\Scripts\python.exe -m pip install -r src\backend\requirements.txt
 echo Backend requirements installed/updated successfully!
-pause
